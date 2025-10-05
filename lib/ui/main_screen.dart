18a35680ca6ea97326c33e5e018ce02a7e1108f2
Manish <mann_9898771157@yahoo.com>
2025-10-05 19:37:58 +0530

    removed initial code and added MainScreen

---
+++ lib/ui/main_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:movies/main.dart';
+
+
+class MainScreen extends StatefulWidget {
+    const MainScreen({super.key});
+
+    @override 
+    State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> { 
+  @override
+  Widget build(BuildContext context) { 
+    return const Placeholder();
+  }
+} 
